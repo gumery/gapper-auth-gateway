2153471a711a3ee5beeaae75d42ea7a3e1c73dd0
Jia Huang <iamfat@gmail.com>
2016-06-16 15:29:00 +0800

    add array_values

--- class/Gini/Controller/CGI/AJAX/Gapper/Auth/Gateway/Location.php
+++ class/Gini/Controller/CGI/AJAX/Gapper/Auth/Gateway/Location.php
@@ -8,15 +8,15 @@ class Location extends \Gini\Controller\CGI
     {
         $data = (array) \Gini\Gapper\Auth\Gateway::getCampuses();
         return \Gini\IoC::construct('\Gini\CGI\Response\JSON', [
-            'data'=> $data
+            'data'=> array_values($data)
         ]);
     }
 
-    public function actionGetBuildings($campus)
+    public function actionGetBuildings($campus=0)
     {
         $data = (array) \Gini\Gapper\Auth\Gateway::getBuildings($campus);
         return \Gini\IoC::construct('\Gini\CGI\Response\JSON', [
-            'data'=> $data
+            'data'=> array_values($data)
         ]);
     }
 }
