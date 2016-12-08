75773aa1e459cf332ca46b71d8f3f23954f69556
pihizi <pihizi@msn.com>
2016-12-08 08:31:04 +0000

    room是一个特殊的存在

--- class/Gini/Controller/CGI/AJAX/Gapper/Auth/Gateway/Location.php
+++ class/Gini/Controller/CGI/AJAX/Gapper/Auth/Gateway/Location.php
@@ -23,8 +23,12 @@ class Location extends \Gini\Controller\CGI
     public function actionGetRooms($building=0)
     {
         $data = (array) \Gini\Gapper\Auth\Gateway::getRooms(['building'=>$building]);
+        $new = [];
+        foreach ($data as $d) {
+            $new[$d['name']] = $d['name'];
+        }
         return \Gini\IoC::construct('\Gini\CGI\Response\JSON', [
-            'data'=> array_values($data)
+            'data'=> array_values($new)
         ]);
     }
 }
