c529c2e343ce9131c7092c278d0fdae8611c0d39
pihizi <pihizi@msn.com>
2017-02-24 17:09:03 +0800

    增加用户登录自动绑定应用的功能

---
+++ class/Gini/Controller/CGI/AJAX/Gapper/Auth/Gateway/Step/User401.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Gini\Controller\CGI\AJAX\Gapper\Auth\Gateway\Step;
+
+class User401 extends \Gini\Controller\CGI
+{
+    public function __index()
+    {
+        if (!\Gini\Gapper\Client::getUserName()) {
+            return $this->_showError();
+        }
+
+        $appIds = (array) \Gini\Config::get('app.auto_install_apps_for_user');
+        if (empty($appIds)) {
+            return \Gini\IoC::construct('\Gini\CGI\Response\JSON', T('您无权访问该应用，请联系系统管理员'));
+        }
+        $myClientID = \Gini\Gapper\Client::getId();
+        if ($myClientID && !in_array($myClientID, $appIds)) {
+            return \Gini\IoC::construct('\Gini\CGI\Response\JSON', T('您无权访问该应用，请联系系统管理员'));
+        }
+
+        $gapperRPC = \Gini\Gapper\Client::getRPC();
+        if (!$gapperRPC->gapper->app->installTo($appId, 'user', (int) $gid)) {
+            return \Gini\IoC::construct('\Gini\CGI\Response\JSON', T('您无权访问该应用，请联系系统管理员'));
+        }
+
+
+        return \Gini\IoC::construct('\Gini\CGI\Response\JSON', true);
+    }
+
+}
+
+
