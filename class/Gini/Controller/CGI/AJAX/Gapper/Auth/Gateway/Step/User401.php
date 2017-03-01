9b87c23efdabc2dc5199d10e3e3964875a2fca2b
pihizi <pihizi@msn.com>
2017-03-01 11:43:18 +0800

    错误提醒要可以区分

--- class/Gini/Controller/CGI/AJAX/Gapper/Auth/Gateway/Step/User401.php
+++ class/Gini/Controller/CGI/AJAX/Gapper/Auth/Gateway/Step/User401.php
@@ -13,15 +13,15 @@ class User401 extends \Gini\Controller\CGI
 
         $appIds = (array) \Gini\Config::get('app.auto_install_apps_for_user');
         if (empty($appIds)) {
-            return \Gini\IoC::construct('\Gini\CGI\Response\JSON', T('您无权访问该应用，请联系系统管理员'));
+            return \Gini\IoC::construct('\Gini\CGI\Response\JSON', T('您无权访问该应用，请联系系统管理员.'));
         }
         $myClientID = \Gini\Gapper\Client::getId();
         if ($myClientID && !in_array($myClientID, $appIds)) {
-            return \Gini\IoC::construct('\Gini\CGI\Response\JSON', T('您无权访问该应用，请联系系统管理员'));
+            return \Gini\IoC::construct('\Gini\CGI\Response\JSON', T('您无权访问该应用，请联系系统管理员!'));
         }
 
         $gapperRPC = \Gini\Gapper\Client::getRPC();
-        if (!$gapperRPC->gapper->app->installTo($appId, 'user', $user['id'])) {
+        if (!$gapperRPC->gapper->app->installTo($myClientID, 'user', $user['id'])) {
             return \Gini\IoC::construct('\Gini\CGI\Response\JSON', T('您无权访问该应用，请联系系统管理员'));
         }
 
